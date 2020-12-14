d2a8641ab0f1a28fad2ad6c484bc02c156a1154b
Kenneth Cheung <kcheung331@gmail.com>
2020-12-14 11:50:40 -0800

    add spotting formulas

--- src/gridfire/fire_spread.clj
+++ src/gridfire/fire_spread.clj
@@ -2,6 +2,7 @@
 (ns gridfire.fire-spread
   (:require [clojure.core.matrix :as m]
             [clojure.core.matrix.operators :as mop]
+            [gridfire.common :refer [sample-at fuel-moisture]]
             [gridfire.crown-fire :refer [crown-fire-eccentricity
                                          crown-fire-line-intensity
                                          cruz-crown-fire-spread
@@ -142,41 +143,6 @@
                                        overflow-heat
                                        0.0))
      :crown-fire?         crown-fire?}))
-
-(defn calc-emc
-  "Computes the Equilibrium Moisture Content (EMC) from rh (relative
-   humidity in %) and temp (temperature in F)."
-  [rh temp]
-  (/ (cond (< rh 10)  (+ 0.03229 (* 0.281073 rh) (* -0.000578 rh temp))
-           (< rh 50)  (+ 2.22749 (* 0.160107 rh) (* -0.01478 temp))
-           :otherwise (+ 21.0606 (* 0.005565 rh rh) (* -0.00035 rh temp) (* -0.483199 rh)))
-     30))
-
-(defn matrix-value-at [[i j] global-clock matrix]
-  (if (> (m/dimensionality matrix) 2)
-    (let [band (int (quot global-clock 60.0))] ;Assuming each band is 1 hour
-      (m/mget matrix band i j))
-    (m/mget matrix i j)))
-
-(defn sample-at
-  [here global-clock matrix multiplier perturb-info]
-  (let [cell       (if multiplier
-                     (map #(quot % multiplier) here)
-                     here)
-        value-here (matrix-value-at cell global-clock matrix)]
-    (if perturb-info
-      (if-let [freq (:frequency perturb-info)]
-        (+ value-here (perturbation/value-at perturb-info matrix cell (quot global-clock freq)))
-        (+ value-here (perturbation/value-at perturb-info matrix cell)))
-      value-here)))
-
-(defn fuel-moisture [relative-humidity temperature]
-  (let [equilibrium-moisture (calc-emc relative-humidity temperature)]
-    {:dead {:1hr   (+ equilibrium-moisture 0.002)
-            :10hr  (+ equilibrium-moisture 0.015)
-            :100hr (+ equilibrium-moisture 0.025)}
-     :live {:herbaceous (* equilibrium-moisture 2.0)
-            :woody      (* equilibrium-moisture 0.5)}}))
 
 (defn extract-constants
   [{:keys [landfire-layers wind-speed-20ft wind-from-direction temperature
