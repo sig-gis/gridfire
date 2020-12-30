600c27f6102203cb0a3dae4a80c410957b1ea006
Kenneth Cheung <kcheung331@gmail.com>
2020-12-30 10:19:36 -0800

    misc fixes unit conversion

--- src/gridfire/spotting.clj
+++ src/gridfire/spotting.clj
@@ -148,7 +148,7 @@
    wind-speed-20ft
    temperature
    [i j]]
-  (let [intensity     (m/mget fire-line-intensity-matrix i j)
+  (let [intensity     (convert/Btu-ft-s->kW-m (m/mget fire-line-intensity-matrix i j))
         froude        (froude-number intensity
                                      wind-speed-20ft
                                      temperature
@@ -199,7 +199,11 @@
    firebrand-count-matrix
    fire-spread-matrix]
   (when crown-fire?
-    (let [deltas (sample-wind-dir-deltas constants spot-config wind-speed-20ft temperature cell)]
+    (let [deltas (sample-wind-dir-deltas constants
+                                         spot-config
+                                         (convert/mph->mps (wind-speed-20ft))
+                                         (convert/F->K temperature)
+                                         cell)]
       (doseq [[x y] (firebrands deltas wind-from-direction cell cell-size)
               :when (in-bounds? num-rows num-cols [x y])]
         (let [count (m/mget firebrand-count-matrix x y)]
